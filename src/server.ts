20a9328e6ea34ca3bc572598b1c83d04b6efded0
kamalMohamed <kmohamed@turndigital.net>
2022-02-15 08:18:39 +0200

    add body barser

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import express, { Request, Response } from 'express'
+import bodyParser from 'body-parser';
+import express, { Request, Response   } from 'express'
 import morgan from "morgan";
 import addAppRoutes from './handlers/index';
 const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
-
+const address = "0.0.0.0:3000"
+app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
