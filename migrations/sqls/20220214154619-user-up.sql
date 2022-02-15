a4a6321b8ad1cc0ffd7fe275f6605e9b1634b535
kamalMohamed <kmohamed@turndigital.net>
2022-02-15 08:14:14 +0200

    models layer add users

---
+++ migrations/sqls/20220214154619-user-up.sql
@@ -0,0 +1,3 @@
+CREATE TABLE IF NOT EXISTS users(id serial PRIMARY KEY 
+,firstname VARCHAR(50) NOT NULL , lastname VARCHAR(50) NOT NULL 
+, password VARCHAR(100) NOT NULL );
---
+++ src/models/test/UserStoreSpec.ts
@@ -0,0 +1,40 @@
+import { UserStore , UserDataBase , User } from './../UserStore';
+
+describe('user store suite', () => {
+    const store = new UserStore();
+    let userId:number;
+    let postUser:User;
+    let updateUser:UserDataBase;
+    it('should have index method', () => {
+        expect(store.index).toBeDefined();
+    });
+    it('index should return a empty users', async () => {
+        const products = await store.index();
+        expect(products).toEqual([]);
+    });
+    it('create should return a user', async () => {
+        postUser = {
+            firstname:'kamal',
+            lastname:'korney',
+            password:'123456'
+        }
+        const {id , ...user} = await store.create(postUser) as UserDataBase;
+        userId = id;
+        expect(user).toEqual(postUser);
+        expect(id).toBe(1);
+    });
+    it('update should return a updated user values', async () => {
+        updateUser = {
+            id : userId ,
+            firstname : "mohamed" ,
+            lastname : "hussein" ,
+            password:"asdasd"
+        }
+        const updatedUser = await store.update(updateUser) as UserDataBase;
+        expect(updateUser).toEqual(updatedUser);
+    });
+    it('delete should remove created user', async () => {
+        const deletedUser = await store.delete(userId) as UserDataBase;
+        expect(deletedUser).toEqual(updateUser);
+    });
+});
