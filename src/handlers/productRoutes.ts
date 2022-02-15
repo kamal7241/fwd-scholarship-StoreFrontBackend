52926808e3e2bfcd8e907eaa17954e41ebae76ef
kamalMohamed <kmohamed@turndigital.net>
2022-02-15 08:16:19 +0200

    handler layers bind product handler with products data layer

--- src/handlers/productRoutes.ts
+++ src/handlers/productRoutes.ts
@@ -1,23 +1,53 @@
-import express, { Request ,  Response } from "express";
+import { ProductStore, ProductDataBase , Product} from './../models/ProductStore';
+import { NextFunction, Request ,  Response } from "express";
 import { Router } from "express";
 const productRouter = Router();
-const index = (_req : Request , res:Response)=>{
-    res.json("get all products");
+const index = async (_req : Request , res:Response , next:NextFunction)=>{
+    try {
+        const products = await new ProductStore().index()
+        res.json({data: products});
+         } catch (error) {
+        next(error)
+    }
 }
-const show = (_req : Request , res:Response)=>{
-    res.json("get an product");
+const show = async(req : Request , res:Response , next:NextFunction)=>{
+    try {
+        const product = await new ProductStore().show(parseInt(req.params.id))
+        res.json({data: product});
+    } catch (error) {
+        next(error)
+    }
 }
 
-const create = (_req : Request , res:Response)=>{
-    res.json("create");
+const create =  async(req : Request , res:Response  , next:NextFunction)=>{
+    const prod = req.body;
+    try {
+        const product = await new ProductStore().create(prod)
+        res.json({data: product});
+    } catch (error) {
+        next(error)
+    }
 }
-const update = (_req : Request , res:Response)=>{
-    res.json("update");
+const update = async(req : Request , res:Response, next:NextFunction)=>{
+    const prod = (req.body) as Product;
+    const updateproduct:ProductDataBase = {...prod , id: parseInt(req.params.id)}
+    try {
+        const product = await new ProductStore().update(updateproduct)
+        res.json({data: product});
+    } catch (error) {
+        next(error)
+    }
 }
-const deleteProduct = (_req : Request , res:Response)=>{
-    res.json("delete 1");
+const deleteProduct = async (req : Request , res:Response, next:NextFunction)=>{
+    try {
+        const product = await new ProductStore().delete(parseInt(req.params.id))
+        res.json({data: product});
+    } catch (error) {
+        next(error)
+    }
 }
 const destroy = (_req : Request , res:Response)=>{
+    // todo add remove all
     res.json("destroy all");
 }
 const addProductRoutes = ():Router=>{
