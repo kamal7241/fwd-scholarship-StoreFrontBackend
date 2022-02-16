aa3f55eef3412d1969409ed4a8117595683a5a2b
kamal7241 <kamal.korney@gmail.com>
2022-02-16 12:45:37 +0200

    cascade forign keys

--- migrations/sqls/20220214172757-order-products-up.sql
+++ migrations/sqls/20220214172757-order-products-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS order_products (
     id serial PRIMARY KEY,
     quantity integer,
-    order_id bigint REFERENCES orders(id),
-    product_id bigint REFERENCES products(id)
+    order_id bigint REFERENCES orders(id) ON DELETE CASCADE,
+    product_id bigint REFERENCES products(id) ON DELETE CASCADE
 );
